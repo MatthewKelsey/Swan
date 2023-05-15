dfe4658251052d950a96ad75d1aae32890466657
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-05-15 14:46:04 +0100

    nav-bar icons

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../api";
+import homeImg from '../icons/home.png'
+import logoutImg from '../icons/logout.png'
+
 function Navbar() {
   const navigate = useNavigate();
+
   function returnHome() {
     navigate("/home");
   }
+
   function exit() {
     logout();
     navigate("/login");
@@ -14,11 +19,11 @@ function Navbar() {
   return (
     <div className="nav-bar">
       <div className="nav-menu" onClick={returnHome}>
-        <h4>Home</h4>
+        <img src={homeImg} alt="Home" className="nav-icon" />
       </div>
-      <h1>Swan Odds </h1>
+      <h1>The Gee-Gees </h1>
       <div className="nav-menu" onClick={exit}>
-        <h4>Logout</h4>
+        <img src={logoutImg} alt="Logout" className="nav-icon" />
       </div>
     </div>
   );
